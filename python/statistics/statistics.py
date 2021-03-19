cf417ab64390c1769cfbf7f96c9e803f4bd87f8c
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-03-19 22:05:52 +0000

    Changed order to reflect the yahoo finance page

--- python/statistics/statistics.py
+++ python/statistics/statistics.py
@@ -5,6 +5,16 @@ import logging
 from python.common import yahoo
 
 """
+-- Valuation measures
+-- Profitability
+'profitMargins': {'raw': 0.03746, 'fmt': '3.75%'},
++ Operating margin (financialData.operatingMargins)
+
+-- Management effectiveness (last year)
+-- Income statement (last year)
+-- Balance Sheet (last quarter)
+-- Cash flow statement (last year)
+
 -- Share statistics
 'sharesOutstanding': {'raw': 94496800, 'fmt': '94.5M', 'longFmt': '94,496,800'},
 'floatShares': {'raw': 91625226, 'fmt': '91.63M', 'longFmt': '91,625,226'},
@@ -16,16 +26,6 @@ from python.common import yahoo
 'shortPercentOfFloat': {'raw': 0.0307, 'fmt': '3.07%'},
 'sharesPercentSharesOut': {'raw': 0.0298, 'fmt': '2.98%'},
 'sharesShortPriorMonth': {'raw': 3801824, 'fmt': '3.8M', 'longFmt': '3,801,824'},
-
--- Profitability
-'profitMargins': {'raw': 0.03746, 'fmt': '3.75%'},
-+ Operating margin (financialData.operatingMargins)
-
--- Valuation measures
--- Management effectiveness (last year)
--- Income statement (last year)
--- Balance Sheet (last quarter)
--- Cash flow statement (last year)
 """
 
 
@@ -34,7 +34,15 @@ class Statistics (yahoo.Yahoo):
 	URL = "https://uk.finance.yahoo.com/quote/{sym}/key-statistics?p={sym}"
 
 	jsonMAP = { \
-				"demo"			: """context.dispatcher.stores.AdStore.adFetchEvent.ps""",
+				#-- Valuation measures
+				# -- Profitability
+				"profitMargins"				: """context.dispatcher.stores.QuoteSummaryStore.defaultKeyStatistics.'profitMargins'.fmt""",
+				"operatingMargins"			: """context.dispatcher.stores.QuoteSummaryStore.financialData.'operatingMargins'.fmt""",
+
+				#-- Management effectiveness (last year)
+				#-- Income statement (last year)
+				#-- Balance Sheet (last quarter)
+				#-- Cash flow statement (last year)
 				# -- Share statistics
 				"sharesOutstanding"			: """context.dispatcher.stores.QuoteSummaryStore.defaultKeyStatistics.'sharesOutstanding'.fmt""",
 				"floatShares"				: """context.dispatcher.stores.QuoteSummaryStore.defaultKeyStatistics.'floatShares'.fmt""",
@@ -46,14 +54,8 @@ class Statistics (yahoo.Yahoo):
 				"shortPercentOfFloat"		: """context.dispatcher.stores.QuoteSummaryStore.defaultKeyStatistics.'shortPercentOfFloat'.fmt""",
 				"sharesPercentSharesOut"	: """context.dispatcher.stores.QuoteSummaryStore.defaultKeyStatistics.'sharesPercentSharesOut'.fmt""",
 				"sharesShortPriorMonth"		: """context.dispatcher.stores.QuoteSummaryStore.defaultKeyStatistics.'sharesShortPriorMonth'.fmt""",
-				# -- Profitability
-				"profitMargins"				: """context.dispatcher.stores.QuoteSummaryStore.defaultKeyStatistics.'profitMargins'.fmt""",
-				"operatingMargins"			: """context.dispatcher.stores.QuoteSummaryStore.financialData.'operatingMargins'.fmt""",
-				#-- Valuation measures
-				#-- Management effectiveness (last year)
-				#-- Income statement (last year)
-				#-- Balance Sheet (last quarter)
-				#-- Cash flow statement (last year)
+				# DEMO
+				"demo"			: """context.dispatcher.stores.QuoteSummaryStore.financialData""",
 			}
 
 	def __init__(self, symbol):

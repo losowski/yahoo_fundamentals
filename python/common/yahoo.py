faafa1356909fe0ccdcdcbfcd997aa0225b24523
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-03-19 21:01:43 +0000

    Demo works

--- python/common/yahoo.py
+++ python/common/yahoo.py
@@ -5,7 +5,7 @@
 import logging
 import re
 import json
-from jsonpath_ng import jsonpath, parse
+import jsonpath_ng
 
 from python.http import http_library
 
@@ -19,7 +19,6 @@ class Yahoo (http_library.HTTPLibrary):
 		super(Yahoo, self).__init__(URL, symbol)
 		self.logger			=	logging.getLogger('Yahoo')
 		self.jsonData		=	None
-		self.jsonString		=	None
 
 
 	def __del__(self):
@@ -38,13 +37,14 @@ class Yahoo (http_library.HTTPLibrary):
 			self.logger.debug("JSON Match: \"%s\"", reMatch)
 			# If it is a match - store the json in the object
 			if (reMatch != None):
-				jsonMatch = reMatch.group('json_data')
-				jsonData = jsonMatch.replace(';','')
-				self.jsonString = jsonData
+				jMatch = reMatch.group('json_data')
+				jData = jMatch.replace(';','')
 				#self.logger.debug("JSON Data: \"%s\"", jsonData)
 				#print ("{json}".format(json=jsonData))
 				# Convert the data into a JSON object
-				self.jsonData = json.loads(jsonData)
+				self.jsonData = json.loads(jData)
+				#self.logger.info("jsonData = \"%s\"", self.jsonData.keys())
+
 
 	#Get Key
 	def get(self, key):
@@ -52,14 +52,11 @@ class Yahoo (http_library.HTTPLibrary):
 		jsonKey = self.jsonMAP.get(key,"")
 		self.logger.debug("jsonkey: \"%s\"", jsonKey)
 		#Get json Expression
-		jsonExpression = parse(jsonKey)
+		jsonExpression = jsonpath_ng.parse(jsonKey)
 		# Get the data
-		matches = jsonExpression.find(self.jsonString)
-		self.logger.info("json: %s (\"%s\"): %s", key, jsonKey, matches)
-		#for match in matches:
-		#	value = match.value
-		#	self.logger.debug("%s (\"%s\"): %s", key, jsonkey, value)
-		return matches
+		matches = jsonExpression.find(self.jsonData)
+		#self.logger.info("json: %s (\"%s\"): %s", key, jsonKey, matches.value)
+		return [ match.value for match in matches ]
 
 	#debug
 	def debug(self):

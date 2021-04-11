bb4b7b13231137bed770ef11d0177f6849a007c4
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-04-11 22:45:53 +0100

    Added basic yahoo_times

---
+++ common/yahoo_times.py
@@ -0,0 +1,14 @@
+# Yahoo Timestamps
+
+import logging
+
+class YahooTimeStamp:
+	def __init__(self):
+		self.timestamp2010	=	1262304000
+		self.timestamp2000	=	946684800
+		pass
+
+	def __del__(self):
+		pass
+
+		

53e9c4d8adeee400d9fe0ef481d944de14e1905f
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-04-12 18:41:57 +0100

    Added yahooTime calculations

--- common/yahoo_times.py
+++ common/yahoo_times.py
@@ -1,11 +1,14 @@
 # Yahoo Timestamps
 
 import logging
+import time
+import calendar
 
 class YahooTimeStamp:
 	def __init__(self):
 		self.timestamp2010	=	1262304000
 		self.timestamp2000	=	946684800
+		self.epochNow		=	int(calendar.timegm(time.gmtime()))
 		pass
 
 	def __del__(self):

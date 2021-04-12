8a765899c5fb50d3826f28bae1450e1a78368101
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2021-04-12 19:07:02 +0100

    Added fundamentals

---
+++ fundamentals_base.py
@@ -0,0 +1,44 @@
+# Financials
+import logging
+
+from yhfundamentals import financials
+from yfundamentals import statistics
+
+
+class FundamentalsBase (object):
+
+	FINANCIALS	=	"financials"
+	STATISTICS	=	"statistics"
+
+	initMap =	{
+					FINANCIALS	:	financials.Financials,
+					STATISTICS	:	statistics.Statistics
+				}
+
+	def __init__(self, symbol):
+		self.logger		=	logging.getLogger('FinancialsBase')
+		self.symbol		=	symbol
+		self.objMap		=	dict() #KeyName : object
+
+	def __del__(self):
+		pass
+
+
+	# Initialise/return object
+	def getRequestObject(objectKey):
+		self.logger.debug("objectKey = %s", objectKey)
+		obj = None
+		# Check if object exists
+		if (objectKey in self.objMap):
+			self.logger.debug("%s found", objectKey)
+			obj = self.objMap[objectKey]
+		else:
+			self.logger.debug("%s NOT found, initialising", objectKey)
+			# If not exists, initialise
+			fn = initMap[objectKey]
+			obj = fn(self.symbol)
+			# Perform initalisation
+			obj.request()
+			obj.parse()
+			self.objMap[objectKey] = obj
+		return obj
